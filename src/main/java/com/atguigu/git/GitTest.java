0b9bdfbd99666c79210d411e3a4639caec58d763
kk <kuangk8@163.com>
2022-06-11 15:45:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author: kk
+ * @date: 2022/6/11 15:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git-test");
+    }
+}
